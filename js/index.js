1783382d77762dd3a770c18854c7084eb6206ce6
chamex23 <monteirokaka15@gmail.com>
2024-04-01 18:46:22 -0300

    feat: javascript upgrade

--- js/index.js
+++ js/index.js
@@ -1,23 +1,34 @@
 const el = document.querySelector("#text");
 const text = "Bem-vindo(a) ao meu portfolio";
 const interval = 200;
+const pauseBetweenIterations = 2000;
 
 function showText(el, text, interval) {
-
-  const char = text.split("").reverse();
+  let index = 0;
+  let direction = 1;
+  let typing = true; // Variável para controlar se está digitando ou apagando
 
   const typer = setInterval(() => {
+    el.innerHTML = text.substring(0, index);
 
-    if(!char.length){
-      return clearInterval(typer);
+    if (typing) {
+      index++;
+      if (index > text.length) {
+        typing = false;
+        setTimeout(() => {
+          direction = -1;
+        }, pauseBetweenIterations);
+      }
+    } else {
+      index--;
+      if (index === 0) {
+        typing = true;
+        setTimeout(() => {
+          direction = 1;
+        }, pauseBetweenIterations);
+      }
     }
-
-    const next = char.pop();
-
-    el.innerHTML += next;
-
-  }, interval)
-
+  }, interval);
 }
 
-showText(el, text, interval)
+showText(el, text, interval);
